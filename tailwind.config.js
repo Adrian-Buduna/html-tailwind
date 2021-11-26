1db08e5f10197634d2b40f5641c5229d6267c187
Adrian-Buduna <adrian.buduna@gmail.com>
2021-11-26 18:05:26 +0200

    "i make footer"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': " #4a4a4a",
+      'secondary': '#ffed4a',
+      'danger': '#e3342f',
+     }),
+    
   },
   variants: {
     extend: {},
